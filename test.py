80fd5b749a8788a38e056d7040433db1c591c19b
Christophe Junke <junke.christophe@gmail.com>
2019-09-04 23:26:47 +0200

    actually do something inside the loop

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import cProfile
 
 def run():
     p = cProfile.Profile()
+    counter = 0
     p.enable()
     for i in range(1, 3000):
         for i in range(1, 75):
             with open("Makefile") as file:
                 for line in file:
-                    pass
+                    counter += 1
     p.disable()
     p.print_stats(sort='cumulative')
+    return counter
